7e5ac6c266af384d3dfd2727bd203e71e47ab941
Steven <srbaesa@gmail.com>
2021-03-16 16:42:58 +0800

    Student

---
+++ Student.java
@@ -0,0 +1,26 @@
+class Student {
+private int IDnum;
+private int hours;
+private int points;
+private static int lastStudentID;
+ }
+public void setIDnum()
+{
+lastStudentID += 1;
+IDnumber = lastStudentID;
+}
+ 
+public int getIDnum()
+{
+return IDnum;
+}
+ 
+public void setHours(int num)
+{
+hours = num;
+}
+ 
+public int getHours()
+{
+return hours;
+}
